b89301d6596b6d5660db375cc6c7a63d74987e94
huyuzhang-lab <1600012808@pku.edu.cn>
2019-05-20 21:43:10 +0800

    First Commit

---
+++ source/Lib/TLibCommon/hyz.h
@@ -0,0 +1,15 @@
+#ifndef H_HYZ
+#define H_HYZ
+
+
+
+// Part for the defination of the macro
+// #define HYZ_NO_SCALE
+#define HYZ_PU_T_MERGE_FLAG
+// Part for the defination of the macro
+
+
+
+
+
+#endif
